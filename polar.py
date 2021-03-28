241d1c24c784fe82d78950a78cc5575499d7c262
Cheetos1131 <68630002+Cheetos1131@users.noreply.github.com>
2021-03-28 18:51:39 -0400

    Add files via upload

--- polar.py
+++ polar.py
@@ -1,28 +1,34 @@
 import math, turtle, subprocess
 
-screen = turtle.Screen()
-screen.setup(width=800, height=800, startx=0, starty=0)
-screen.bgcolor('black')
-pen = turtle.Turtle()
-pen.speed(20)
-n = 5
+screen1 = turtle.Screen()
+screen2 = turtle.Screen()
+screen1.setup(width=1080, height=1920, startx=0, starty=0)
+screen2.setup(width=1080, height=1920, startx=0, starty=0)
+screen1.bgcolor('black')
+screen2.bgcolor('black')
+rose = turtle.Turtle()
+rose.speed(20)
+startDeg = 0
+endDeg = 360
+n = 100
 d = 97
 
-pen.color('blue')
-pen.pensize(0.5)
-for theta in range(361):
-    k = theta * d * math.pi / 180
-    r = 300 * math.sin(n * k)
-    x = r * math.cos(k)
-    y = r * math.sin(k)
-    pen.goto(x, y)
+rose.color('blue')
+rose.pensize(0.5)
+for theta in range(startDeg, endDeg + 1, 1):
+    k = theta * d * math.pi / 180 # Conversion to radian degrees
+    r = 300 * math.sin(n * k) # Getting the radius
+    x = r * math.cos(k) # Getting the X coordinate
+    y = r * math.sin(k) # Getting the Y coordinate
+    rose.goto(x, y) # Setting the point to be drawn
+rose.goto(0,0) # Reset point for the rose curve drawing
 
-pen.color('red')
-pen.pensize(4)
-for theta in range(361):
+rose.color('red')
+rose.pensize(4)
+for theta in range(startDeg, endDeg + 1, 1):
     k = theta * math.pi / 180
     r = 300 * math.sin(n * k)
     x = r * math.cos(k)
     y = r * math.sin(k)
-    pen.goto(x, y)
-screen.exitonclick()
+    rose.goto(x, y)
+screen1.exitonclick()
