1908af912baa936ad2edc8e8a1ba15d66c3f1dd4
Cheetos1131 <68630002+Cheetos1131@users.noreply.github.com>
2021-03-29 21:17:12 -0400

    Add files via upload
    Added the blue plot lines to work for the cardioid by introducing the variable "d".

--- Cardioid.py
+++ Cardioid.py
@@ -5,12 +5,12 @@ screen2.setup(width=1080, height=1920, startx=0, starty=0)
 turtle.title("Cardioid")
 screen2.bgcolor('black')
 cardioid = turtle.Turtle()
-cardioid.pensize(4)
-cardioid.color("red")
+cardioid.speed(200)
 
 startDeg = 0
 endDeg = 360
 r = 75
+d = 97
 
 def Cardioid(startDeg, endDeg, r):
     
@@ -19,6 +19,18 @@ def Cardioid(startDeg, endDeg, r):
     cardioid.pendown()
     cardioid.hideturtle()
 
+
+    cardioid.pensize(0.5)
+    cardioid.color("blue")
+    for degrees in  range(startDeg, endDeg + 1): 
+        rads = degrees * d * math.pi/180
+        distance = (1 + math.cos(rads)) * r
+        x = math.cos(rads) * distance
+        y = math.sin(rads) * distance
+        cardioid.goto(x, y)
+    
+    cardioid.pensize(4)
+    cardioid.color("red")
     for degrees in range(startDeg, endDeg + 1):
         rads = math.radians(degrees)
         distance = (1 + math.cos(rads)) * r
